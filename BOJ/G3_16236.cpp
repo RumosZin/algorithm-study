a09b36d92d8c45dc5a0326fba8c1e98dd65487ad
yunjin Kim <81238093+RumosZin@users.noreply.github.com>
2024-04-11 23:22:55 +0900

    April 11

--- BOJ/G3_16236.cpp
+++ BOJ/G3_16236.cpp
@@ -1,90 +1,135 @@
-//#include <iostream>
-//#include <queue>
-//
-//using namespace std;
-//
-//int n;
-//int map[22][22];
-//int dx[4] = { 0, -1, 1, 0 };
-//int dy[4] = { -1, 0, 0, 1 };
-//
-//int bx, by;
-//int result = 0; // 총 걸린 시간
-//int cnt = 0; // 물고기 먹은 횟수
-//int sz = 2; // 상어 사이즈
-//
-//bool stop = false; // 먹을 물고기가 없는 경우
-//bool eat = false; // 한 마리를 먹은 경우
-//
-//void bfs(int a, int b, bool visit[][22], int shsize) {
-//	queue<pair<pair<int, int>, int>> q;
-//	q.push(make_pair(make_pair(a, b), 0));
-//	visit[b][a] = true;
-//	int temp; // 한 마리를 먹는데 걸린 시간
-//
-//	while (!q.empty()) {
-//		int x = q.front().first.first;
-//		int y = q.front().first.second;
-//		int count = q.front().second;
-//
-//		// 가장 위쪽을 먹고 그 다음 왼쪽을 먹음
-//		if (map[y][x] > 0 && map[y][x] < shsize && temp == count) {
-//			if ((by > y) || (by == y && bx > x)) {
-//				by = y; bx = x; continue;
-//			}
-//		}
-//		q.pop();
-//
-//		for (int i = 0; i < 4; i++) {
-//			int nx = x + dx[i];
-//			int ny = y + dy[i];
-//
-//			if (nx >= 0 && nx < n && ny >= 0 && ny < n && !visit[ny][nx]) {
-//				if (map[ny][nx] <= shsize) { // 지나가거나 먹을 수 있음
-//					if (map[ny][nx] > 0 && map[ny][nx] < shsize && !eat) {
-//						eat = true;
-//						bx = nx;
-//						by = ny;
-//						temp = count + 1;
-//						result += temp;
-//					}
-//					else {
-//						q.push(make_pair(make_pair(nx, ny), count + 1));
-//						//cout << nx << " " << ny << "\n";
-//						visit[ny][nx] = true;
-//					}
-//				}
-//			}
-//		}
-//		//cout << "\n";
-//	}
-//}
-//
-//int main() {
-//	cin >> n;
-//	for (int i = 0; i < n; i++) {
-//		for (int j = 0; j < n; j++) {
-//			cin >> map[i][j];
-//			if (map[i][j] == 9) {
-//				by = i; bx = j; map[i][j] = 0;
-//			}
-//		}
-//	}
-//
-//	while (!stop) {
-//		bool visit[22][22] = { 0 };
-//		bfs(bx, by, visit, sz);
-//		if (eat) { // 먹었을 경우
-//			eat = false;
-//			cnt += 1; // 물고기 먹은 수 증가
-//			map[by][bx] = 0; // 먹은 물고기 삭제
-//			if (cnt == sz) { // 상어 사이즈 증가
-//				sz += 1;
-//				cnt = 0;
-//			}
-//		}
-//		else stop = true;
-//	}
-//
-//	cout << result;
-//}
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int board[21][21];
+int visited[21][21];
+
+typedef struct shark {
+    int row, col, size, fish, dist;
+};
+
+typedef struct fish {
+    int row, col, dist;
+};
+
+queue<shark> q;
+vector<fish> v;
+
+int shark_row;
+int shark_col;
+int shark_size;
+int shark_fish;
+int answer = 0;
+
+int dx[4] = { 0, 0, 1, -1 };
+int dy[4] = { 1, -1, 0, 0 };
+
+bool cmp(fish a, fish b) {
+    if (a.dist < b.dist) return true;
+    else if (a.dist == b.dist) {
+        if (a.row < b.row) return true;
+        else if (a.row == b.row) {
+            if (a.col < b.col) return true;
+            else return false;
+        }
+        else return false;
+    }
+    else return false;
+}
+
+void bfs() {
+    while (!q.empty()) {
+        int row = q.front().row;
+        int col = q.front().col;
+        int size = q.front().size;
+        int fish = q.front().fish;
+        int dist = q.front().dist; 
+        q.pop();
+
+        // can eat?
+        for (int d = 0; d < 4; d++) {
+            int next_row = row + dx[d];
+            int next_col = col + dy[d];
+
+            if (next_row >= 0 && next_row < N
+                && next_col >= 0 && next_col < N
+                && board[next_row][next_col] <= size
+                && visited[next_row][next_col] != 1) {
+                q.push({ next_row, next_col, size, fish, dist + 1 });
+                if (board[next_row][next_col] > 0
+                    && board[next_row][next_col] < size) {
+                    v.push_back({ next_row, next_col, dist + 1});
+                }
+                visited[next_row][next_col] = 1;
+            }
+        }
+
+
+    }
+}
+
+int main() {
+
+    // input
+    cin >> N;
+
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cin >> board[i][j];
+            if (board[i][j] == 9) {
+                shark_row = i;
+                shark_col = j;
+                board[i][j] = 0;
+            }
+        }
+    }
+
+    shark_size = 2;
+    shark_fish = 0;
+
+    while (true) {
+
+        // initialize
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                visited[i][j] = 0;
+            }
+        }
+
+        v.clear();
+
+        // find food bfs
+        q.push({ shark_row, shark_col, shark_size, shark_fish, 0 });
+        visited[shark_row][shark_col] = 1;
+        bfs();
+
+        // sort food v
+        sort(v.begin(), v.end(), cmp);
+
+        // stop condition : nothing to eat fish
+        if (v.size() == 0) {
+            break;
+        }
+
+        // eat and update shark
+        answer += v[0].dist;
+        shark_row = v[0].row;
+        shark_col = v[0].col;
+        if (shark_size == shark_fish + 1) {
+            shark_size += 1;
+            shark_fish = 0;
+        }
+        else {
+            shark_fish++;
+        }
+        board[shark_row][shark_col] = 0;
+    }
+
+    cout << answer;   
+}
