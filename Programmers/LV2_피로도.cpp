65f6dfb6ddc795df008c604a2af1f9081c73515c
rumos <gyunjin3@gmail.com>
2024-09-23 16:14:57 +0900

    Sep 23 PS [피로도] 구현 cpp RE success
    dfs 내부에 for문을 두어 코드를 더 간결히 작성!

--- Programmers/LV2_피로도.cpp
+++ Programmers/LV2_피로도.cpp
@@ -10,25 +10,25 @@ vector<vector<int>> dun;
 int answer = -1;
 int v_size = 0;
 
-void dfs(int d, int k, int count) {
-    visited[d] = true; count++;
+void dfs(int k, int count) {
+    answer = max(answer, count);
     
     for(int i = 0; i < v_size; i++) {
-        if(visited[i]) continue;
-        if(dun[i][0] <= k && !visited[i]) dfs(i, k - dun[i][1], count);
-        else { continue; }
+        if(dun[i][0] <= k && !visited[i]) {
+            visited[i] = true; 
+            dfs(k - dun[i][1], count + 1);
+            visited[i] = false; 
+        }
     }
-    visited[d] = false; answer = max(answer, count);
+    
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
     v_size = dungeons.size();
     dun = dungeons;
-    visited.resize(v_size); 
+    visited.resize(v_size);
     
-    for(int i = 0; i < dungeons.size(); i++) { 
-        dfs(i, k - dungeons[i][1], 0); // 시작, 현재까지 피로도, 현재까지 방문한 던전의 개수
-    }
+    dfs(k, 0); // 현재까지의 피로도, 현재까지 방문한 던전의 개수
     
     return answer;
 }
